c3fca9353718b26d09e23c614f49f75b423e4237
virak0001 <virakcambodia44@gmail.com>
2022-10-24 13:51:20 +0700

    chore: inject user services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
+
+  @InjectRepository(User)
+  private readonly repository: Repository<User>;
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -24,3 +32,4 @@ export class UsersService {
     return `This action removes a #${id} user`;
   }
 }
+
